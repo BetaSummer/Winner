5a3344caf9a448316cf9ef693418d81a1677f310
ahbing <givemearainbowlife@gmail.com>
2016-03-27 20:34:33 +0800

    fix lint

--- proxy/reply.js
+++ proxy/reply.js
@@ -2,12 +2,12 @@
 var Reply = require('../models').Reply;
 
 /*
-* newAndSave 创建新的一个回复内童
-* @param { String } 回复文本内容
-* @param { String } 评论的商品 id
-* @param { String } 创建评论的用户 id
-* @param { String } 回复某条评论的 id，一级评论该值为空字符串
-* @param { Function } 回调函数
+ * newAndSave 创建新的一个回复内童
+ * @param { String } 回复文本内容
+ * @param { String } 评论的商品 id
+ * @param { String } 创建评论的用户 id
+ * @param { String } 回复某条评论的 id，一级评论该值为空字符串
+ * @param { Function } 回调函数
  */
 
 exports.newAndSave = function(content, commodityId, masterId, replyId, cb) {
@@ -20,5 +20,5 @@ exports.newAndSave = function(content, commodityId, masterId, replyId, cb) {
 };
 
 exports.findReplyById = function(id, cb) {
-  Reply.findOne({_id: id}, cb);
+  Reply.findOne({ _id: id }, cb);
 };
